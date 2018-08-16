0aaa761e34ee674a5c30170d70fbd0d76a0d02cf
Jesús Mejía <jesusmejded@gmail.com>
2018-08-16 13:48:49 -0500

    slider

---
+++ js/engine.js
@@ -0,0 +1,30 @@
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("content-banner");
+
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+
+  slides[slideIndex-1].style.display = "table";  
+}
+
+function initSlides(){
+  showSlides(slideIndex);
+  
+  setInterval(function() {
+    slideIndex += 1
+    showSlides(slideIndex);
+  }, 3000)
+}
