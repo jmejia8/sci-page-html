9ce88dd8cc90b387c51ce400bc298660c7b1c88f
Jesus Mejia <jesusmejded@gmail.com>
2018-09-20 18:20:07 -0500

    hide the menu

--- js/engine.js
+++ js/engine.js
@@ -28,3 +28,13 @@ function initSlides(){
     showSlides(slideIndex);
   }, 3000)
 }
+
+function displayMenu() {
+  var menu = document.getElementById('menu');
+
+  if (menu.style.display == 'block') {
+    menu.style.display = 'none'
+  }else{
+    menu.style.display = 'block'
+  }
+}
